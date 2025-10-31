062325353dcf23f52a3b4f11a7a0b8357cdef439
Bernard Chan <allanbernardchan+github@gmail.com>
2025-10-31 23:43:53 +0800

    Update Real MAC addresses for runners and clarify Beninca head MAC comment

--- gate_system/config.py
+++ gate_system/config.py
@@ -2,14 +2,14 @@
 ADMIN_MAC = b"\x1c\x69\x20\xce\xf8\xe4"
 GATE_GUARD_MAC = b'\x1c\x69\x20\xce\xfa\x24'
 INSIDE_READER_MAC = b"\x08\xa6\xf7\xbc\xe5\x48"
-BENINCA_HEAD_MAC = b'\xc8\x2e\x18\x51\xc8\x5c'
+BENINCA_HEAD_MAC = b'\xc8\x2e\x18\x51\xc8\x5c' # Actual board installed
 BACKUP_BUTTON_MAC = b'\x1c\x69\x20\xce\xf7\xe4'
 OUTSIDE_READER_MAC = b"\x1c\x69\x20\xcc\xe0\x34"
-RUNNER_A_MAC = pass
-RUNNER_B_MAC = pass
-RUNNER_C_MAC = pass
-RUNNER_D_MAC = pass
-RUNNER_E_MAC = pass
+RUNNER_A_MAC = b'\x84\x1f\xe8\x32\xe9\x00'
+RUNNER_B_MAC = b'\x88\x57\x21\x21\xae\x04'
+RUNNER_C_MAC = b'\x88\x57\x21\x21\xde\xec'
+RUNNER_D_MAC = b'\x84\x1f\xe8\x36\x70\xd4'
+RUNNER_E_MAC = b'\x84\x1f\xe8\x32\xea\x24'
 
 READER_MACS = {
     INSIDE_READER_MAC,
